7335eb65891a38ec679d6828ca78b6460b96bd83
Pankaj <pankaj08072000@gmail.com>
2025-07-24 17:20:46 +0530

    setup & configure celery and django rest framework

--- pilotlog_project/pilotlog_project/settings.py
+++ pilotlog_project/pilotlog_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pilotlog",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery Configuration
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 LOGGING = {
     "version": 1,
