fc5f542ff33ea8154a9e3007c3ee1bba0defa5e4
Pankaj <pankaj08072000@gmail.com>
2025-07-24 21:18:38 +0530

    fix/ celery task picking incorrect file path

--- pilotlog_project/pilotlog/tasks.py
+++ pilotlog_project/pilotlog/tasks.py
@@ -2,11 +2,16 @@ from celery import shared_task
 from django.core.management import call_command
 
 
+from django.conf import settings
+import os
+
 @shared_task
 def import_data_task(file_path):
-    call_command("import_data", file_path)
+    abs_file_path = os.path.join(settings.BASE_DIR, '..', file_path)
+    call_command("import_data", abs_file_path)
 
 
 @shared_task
 def export_data_task(file_path):
-    call_command("export_data", file_path)
+    abs_file_path = os.path.join(settings.BASE_DIR, '..', file_path)
+    call_command("export_data", abs_file_path)
